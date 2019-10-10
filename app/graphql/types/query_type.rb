69d3e3ba28f241b3d41ae3cb28c918aa9eeb4328
Sammy Nave <sammy@mentorcollective.org>
2019-10-11 00:12:49 +0100

    start-end work

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -31,9 +31,19 @@ module Types
       ::Hang.all
     end
 
-    field :my_hangs, [Types::HangType], null: false
+    field :my_hangs, [Types::HangType], null: false do
+      argument :start_after, GraphQL::Types::ISO8601DateTime, required: false
+      argument :start_before, GraphQL::Types::ISO8601DateTime, required: false
+    end
 
-    def my_hangs
+    def my_hangs(start_after: nil, start_before: nil)
+      return context[:current_user].hangs.where('hangs.start_at > ?', start_after) if start_after
+      return context[:current_user].hangs.where('hangs.start_at < ?', start_before) if start_before
+
+      if start_before && start_after
+        return context[:current_user].hangs.where('hangs.start_at > ? and hangs.end_at < ?', start_after, start_before)
+      end
+
       context[:current_user].hangs
     end
 
